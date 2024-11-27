cee1acecc7daad2f0bf0853c162483fd9fbbed07
Syro13 <lowisjakub@gmail.com>
2024-11-27 21:25:49 +0000

    Implement Courses and Home Page

---
+++ soa_ca2_frontend/src/pages/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Courses() {
+    return (
+      <div>
+        <h1>Courses Page</h1>
+        <p>This is the Courses Page.</p>
+      </div>
+    );
+  }
+
+export default Courses;
