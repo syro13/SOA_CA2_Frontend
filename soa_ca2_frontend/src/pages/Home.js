900c84736ce57bc5ee9a77ea177b9e165c04aba5
Syro13 <lowisjakub@gmail.com>
2024-12-03 23:43:40 +0000

    Add Home page content

--- soa_ca2_frontend/src/pages/Home.js
+++ soa_ca2_frontend/src/pages/Home.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 function Home() {
-    return (
-      <div className="main-container">
-        <h1>Home Page</h1>
-        <p>This is the Home Page.</p>
+  return (
+    <div className="main-container">
+        <div className='content-container'>
+        <h1>Welcome to the Course Management System</h1>
+        <p>
+          Our Educational Course Management System is designed to streamline the process of managing educational courses. Whether you are an administrator looking to manage course offerings or a student seeking to explore available courses, our system provides an intuitive and efficient solution.
+        </p>
+        <p>
+          <strong>Features:</strong>
+        </p>
+        <ul style={{ listStyleType: 'none'}}>
+          <li>Administrators can add, edit, and delete courses.</li>
+          <li>Students can view detailed information about available courses.</li>
+          <li>Responsive and user-friendly interface built with React.</li>
+          <li>Secure and robust backend powered by a RESTful API.</li>
+        </ul>
+        <p>
+          Get started by navigating through the menu to explore the various functionalities of our system. We are committed to providing a seamless and efficient experience for all users.
+        </p>
       </div>
-    );
+    </div>
+  );
   }
 
 export default Home;
