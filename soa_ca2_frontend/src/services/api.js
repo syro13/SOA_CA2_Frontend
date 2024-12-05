be9495256389c574f9bdc7beeed272fbafb31115
Syro13 <lowisjakub@gmail.com>
2024-12-05 18:28:13 +0000

    Change Api base url to deployed backend

--- soa_ca2_frontend/src/services/api.js
+++ soa_ca2_frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5118';
+const API_BASE_URL = 'https://educationcoursemanagementapiapp.azurewebsites.net/';
 const API_KEY = 'S3cR3tK3yForMyAPI@2024*EducationCourse#Management';
 
 // Function to Login
